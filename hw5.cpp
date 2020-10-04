0fa2f7ff9f11a9d9a20669a081c188b2705c9fd9
Masayoshi Sato <yoshi2296@gmail.com>
2020-10-04 03:11:06 +0000

    completed Bubble and Insertion Sort

--- hw5.cpp
+++ hw5.cpp
@@ -1,22 +1,66 @@
 #include "hw5.h"
 
 
-void BubbleSortHelper(std::vector<std::string> &vec, int size) {
-  std::cout << size << std::endl;
+void print(std::vector<int> &v) {
+  std::cout << "[";
+  bool first = true;
+  for (auto x : v) {
+    if (!first) {
+      std::cout << ", ";
+    }
+    std::cout << x;
+    first = false;
+  }
+  std::cout << "]";
+  std::cout << std::endl;
+}
+
+void BubbleSortHelper(std::vector<std::string> &vec, int size, std::string tmp_string) {
   if (size==0) {
     return;
   }
-  std::string tmp_string;
-  for (int index = 0; index <= size; index++) {
+  for (int index = 0; index < size; index++) {
     if (vec[index].compare(vec[index+1]) >= 0) {
       tmp_string = vec[index];
       vec[index] = vec[index+1];
       vec[index+1] = tmp_string;
     }
   }
-  BubbleSortHelper(vec, size-1);
+  BubbleSortHelper(vec, size-1, "");
 }
 
 void BubbleSort(std::vector<std::string> &vec) {
-  BubbleSortHelper(vec, vec.size());
+  if (vec.size() <= 1) {
+    return;
+  }
+  BubbleSortHelper(vec, vec.size()-1, "");
+}
+
+
+
+
+
+void InsertionSortHelper(std::vector<int>& v, int left_index, int key_index, int key_int, int tmp_int) {
+  if (key_index == v.size()-1 && left_index == 0) {
+    return;
+  }
+  else if (left_index == 0) {
+    if (v[left_index] > key_int) {
+      tmp_int = v[left_index];
+      v[left_index] = key_int;
+      v[left_index+1] = tmp_int;
+    }
+    InsertionSortHelper(v, key_index, key_index+1, v[key_index], 0);
+  }
+  else {
+    if (v[left_index] > key_int) {
+      v[left_index+1] = v[left_index];
+      v[left_index] = key_int;
+    }
+    InsertionSortHelper(v, left_index-1, key_index, key_int, 0);
+  }
+}
+
+void InsertionSort(std::vector<int>& v) {
+  InsertionSortHelper(v, 0, 1, v[1], 0);
 }
--- main.cpp
+++ main.cpp
@@ -16,13 +16,31 @@ void print(std::vector<std::string> &v) {
   std::cout << std::endl;
 }
 
+/*void print(std::vector<int> &v) {
+  std::cout << "[";
+  bool first = true;
+  for (auto x : v) {
+    if (!first) {
+      std::cout << ", ";
+    }
+    std::cout << x;
+    first = false;
+  }
+  std::cout << "]";
+  std::cout << std::endl;
+}*/
+
 int main() {
   std::vector<std::string> v;
+  std::vector<int> v_int;
 
   v = {"emma", "dora", "ruth", "ziegellaub", "eichler"};
+  v_int = {6,4,23,-7,9,5,-3,8,6543,3,5,6778,23};
   //std::cout << "Case 1: ";
   //MergeSort(v);
-  BubbleSort(v);
-  //print(v);
+  //BubbleSort(v);
+  InsertionSort(v_int);
+  std::cout << "output: ";
+  print(v_int);
   //std::cout << "Expected: [dora, eichler, emma, ruth, ziegellaub]" << std::endl;
 }
