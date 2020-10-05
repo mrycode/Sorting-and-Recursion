ac4e898ff91ba1f7f1a75437d87fba21435978dc
Masayoshi Sato <yoshi2296@gmail.com>
2020-10-05 01:41:42 +0000

    deleted recursive bubble sort

--- hw5.cpp
+++ hw5.cpp
@@ -2,33 +2,11 @@
 
 
 
-/*
-static void BubbleSortHelper(std::vector<std::string> &vec, int size, std::string tmp_string) {
-  if (size==0) {
-    return;
-  }
-  for (int index = 0; index < size; index++) {
-    if (vec[index].compare(vec[index+1]) >= 0) {
-      tmp_string = vec[index];
-      vec[index] = vec[index+1];
-      vec[index+1] = tmp_string;
-    }
-  }
-  BubbleSortHelper(vec, size-1, "");
-}
-
 void BubbleSort(std::vector<std::string> &vec) {
   if (vec.size() <= 1) {
     return;
   }
-  BubbleSortHelper(vec, vec.size()-1, "");
-}*/
 
-void BubbleSort(std::vector<std::string> &vec) {
-  if (vec.size() <= 1) {
-    return;
-  }
-  
   int arr_end = vec.size()-1;
   std::string tmp_string;
   
